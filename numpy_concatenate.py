77ae2216eff3189ec2c22348a764931e7b0a431b
Vajas Máté <88398933+Letsktor@users.noreply.github.com>
2024-01-29 06:26:21 +0100

    Add files via upload

---
+++ numpy_concatenate.py
@@ -0,0 +1,73 @@
+"""
+Concatenate
+
+Two or more arrays can be concatenated together using the concatenate function with a tuple of the arrays to be joined:
+
+import numpy
+
+array_1 = numpy.array([1,2,3])
+array_2 = numpy.array([4,5,6])
+array_3 = numpy.array([7,8,9])
+
+print numpy.concatenate((array_1, array_2, array_3))    
+
+#Output
+[1 2 3 4 5 6 7 8 9]
+If an array has more than one dimension, it is possible to specify the axis along which multiple arrays are concatenated. By default, it is along the first dimension.
+
+import numpy
+
+array_1 = numpy.array([[1,2,3],[0,0,0]])
+array_2 = numpy.array([[0,0,0],[7,8,9]])
+
+print numpy.concatenate((array_1, array_2), axis = 1)   
+
+#Output
+[[1 2 3 0 0 0]
+ [0 0 0 7 8 9]]    
+Task
+
+You are given two integer arrays of size X and X ( &  are rows, and  is the column). Your task is to concatenate the arrays along axis .
+
+Input Format
+
+The first line contains space separated integers ,  and .
+The next  lines contains the space separated elements of the  columns.
+After that, the next  lines contains the space separated elements of the  columns.
+
+Output Format
+
+Print the concatenated array of size X.
+
+Sample Input
+
+4 3 2
+1 2
+1 2 
+1 2
+1 2
+3 4
+3 4
+3 4 
+Sample Output
+
+[[1 2]
+ [1 2]
+ [1 2]
+ [1 2]
+ [3 4]
+ [3 4]
+ [3 4]] 
+"""
+import numpy
+
+s=input().split()
+li1=[]
+li2=[]
+for i in range(int(s[0])):
+    temp=input().split()
+    li1.append(numpy.array(temp,int))
+for i in range(int(s[1])):
+    temp=input().split()
+    li2.append(numpy.array(temp,int))
+print(numpy.concatenate((li1,li2),axis=0))
